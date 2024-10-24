7069abeed67278e849611428c1e3875ba2c1c5f0
rikukodama <crystalstar0920@gmail.com>
2024-10-24 16:58:38 +0900

    wallet connect button added

--- src/Components/MainVisual.tsx
+++ src/Components/MainVisual.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const MainVisual: React.FC = () => {
     return (
-        <div className='sm:px-28 px-5'>
-            <div className="sm:mt-14 sm:w-3/4 sm:relative sm:h-[830px] w-full">
+        <div className='sm:px-28 px-5 overflow-hidden'>
+            <div className="sm:mt-14 sm:relative sm:h-[830px] w-full xl:w-3/4">
                 <div className='flex sm:flex-col flex-col-reverse gap-5'>
                     <div className='flex gap-10 flex-col sm:flex-row'>
                         <div className='flex gap-2 justify-center'>
@@ -12,16 +12,19 @@ const MainVisual: React.FC = () => {
                             <img alt="Twitter" data-ssr-src-done="true" fetchPriority="high" src="https://static.wixstatic.com/media/9c4b521dd2404cd5a05ed6115f3a0dc8.png/v1/fill/w_39,h_39,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/9c4b521dd2404cd5a05ed6115f3a0dc8.png" className="w-10 h-10 object-cover" />
                             <img alt="GitHub" data-ssr-src-done="true" fetchPriority="high" src="https://static.wixstatic.com/media/11062b_2439efec5ecc4d44b0dc6986da28e4bb~mv2.png/v1/fill/w_39,h_39,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_2439efec5ecc4d44b0dc6986da28e4bb~mv2.png" className="w-10 h-10 object-cover" />
                         </div>
-                        <span className='rounded-full bg-white text-purple shadow-custom-purple py-2 px-4 w-2/3 mx-auto sm:w-52 sm:mx-0'>LIVE-PRE-SALE</span>
-                        <span className='rounded-full bg-white text-purple shadow-custom-purple py-2 px-4 w-2/3 mx-auto sm:w-52 sm:mx-0'>LITE-PAPER</span>
+                        <button className='rounded-full bg-white text-purple shadow-custom-purple py-2 px-4 w-2/3 mx-auto sm:w-52 sm:mx-0'>LIVE-PRE-SALE</button>
+                        <button className='rounded-full bg-white text-purple shadow-custom-purple py-2 px-4 w-2/3 mx-auto sm:w-52 sm:mx-0'>LITE-PAPER</button>
                     </div>
                     <div className='mt-5 flex sm:justify-between w-full sm:relative sm:flex-row flex-col gap-5'>
                         <div className='text-left w-full'>
                             <div className='sm:text-[80px] text-[50px] font-bold font-suez text-darkPurple leading-tight'>
                                 Welcome to <br />GODSWILL <br />Ecosystem
                             </div>
-                            <div className='font-serif text-2xl text-darkPurple' style={{ fontFamily: "sans-serif" }}>
+                            <div className='font-serif text-2xl text-darkPurple mb-6' style={{ fontFamily: "sans-serif" }}>
                                 "Divine Justice Awaits Your Next Move!"
+                            </div>
+                            <div className='text-center lg:inline-block lg:w-fit mx-auto'>
+                                <button className='rounded-full bg-white text-purple shadow-custom-purple py-2 px-4 inline-block w-fitmx-auto'>Connect Wallet</button>
                             </div>
                         </div>
                         <div className='sm:absolute sm:-top-10 sm:right-0 mt-5'>
@@ -35,7 +38,7 @@ const MainVisual: React.FC = () => {
                         </div>
                     </div>
                 </div>
-                <div className='sm:absolute sm:bottom-0 sm:right-10 w-full sm:w-3/4'>
+                <div className='sm:absolute sm:bottom-0 sm:-right-48 w-full'>
                     <img
                         src="https://static.wixstatic.com/media/7a45de2e827c42c4b4a11f7f13a869a5.png/v1/fill/w_720,h_471,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/7a45de2e827c42c4b4a11f7f13a869a5.png"
                         alt=""
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 const NavBar: React.FC = () => {
     return (
         <nav className='h-fit shadow-custom-red rounded-b-xl min-w-72' style={{ fontFamily: 'sans-serif' }}>
-            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4 rounded-bl-xl'>Home</span>
-            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4'>Games</span>
-            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4'>Careers</span>
-            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4'>About</span>
-            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4'>Contact</span>
-            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4'>Blog</span>
-            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4 rounded-br-xl'>More</span>
+            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4 cursor-pointer rounded-bl-xl'>Home</span>
+            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4 cursor-pointer'>Games</span>
+            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4 cursor-pointer'>Careers</span>
+            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4 cursor-pointer'>About</span>
+            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4 cursor-pointer'>Contact</span>
+            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4 cursor-pointer'>Blog</span>
+            <span className='text-black inline-block bg-white hover:text-white hover:bg-mainRed px-4 py-4 cursor-pointer rounded-br-xl'>More</span>
         </nav>
     );
 };
